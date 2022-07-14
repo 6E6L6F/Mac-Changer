de7cd9612f1d888aff8098954a764931929f89db
Elf <87319528+elf-cyber@users.noreply.github.com>
2022-07-14 17:02:28 +0430

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-echo "Coded By @E_L_F_6_6_6\nMy Channel https://T.Me/Elf_Security_Cyber"
+echo "Coded By @E_L_F_6_6_6"
+echo "My Channel https://T.Me/Elf_Security_Cyber"
 chmod 777 Mac
 cp -r Mac /usr/local/bin
 echo "Run Command MAC -ifce= [InterFace Name , Default eth0]  -newMac=00:00:00:00:00"
