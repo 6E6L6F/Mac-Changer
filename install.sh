eb83a520cdc083262e5d675bf270b6d95c37a6e7
Elf <87319528+elf-cyber@users.noreply.github.com>
2022-07-14 17:01:14 +0430

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,4 @@
+echo "Coded By @E_L_F_6_6_6\nMy Channel https://T.Me/Elf_Security_Cyber"
+chmod 777 Mac
+cp -r Mac /usr/local/bin
+echo "Run Command MAC -ifce= [InterFace Name , Default eth0]  -newMac=00:00:00:00:00"
